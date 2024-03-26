230a2bd99cb08a5180662316fec35546a3a54701
Garrett Lewis <115313310+lewisjga@users.noreply.github.com>
2024-03-26 10:43:15 -0500

    Create functions to check for clicks and button values

--- scripts.js
+++ scripts.js
@@ -3,3 +3,21 @@ const opPadWrapper = document.getElementById('operator-pad');
 const padButtons = document.querySelectorAll('.pad-button');
 let display = document.getElementById('calc-display');
 
+padButtons.forEach(el => {
+    el.addEventListener('click', () => {
+        console.log(el.textContent);
+        calcFill(el.textContent, el.classList)
+    })
+});
+
+function calcFill (value, type) {
+    if(type.contains("num")){
+        console.log('Number selected');
+    } else if(type.contains("operator")){
+        console.log('Operator selected');
+    } else if(type.contains("equals")){
+        console.log('Equals selected');
+    } else if(type.contains("clear")){
+        console.log('Clear selected');
+    }
+}
