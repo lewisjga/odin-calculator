631a8d3b6c81c50e8be190e90dd2acd8bc377907
Garrett Lewis <115313310+lewisjga@users.noreply.github.com>
2024-03-26 12:24:26 -0500

    Add calculator functionality

--- scripts.js
+++ scripts.js
@@ -2,6 +2,10 @@ const numPadWrapper = document.getElementById('number-pad');
 const opPadWrapper = document.getElementById('operator-pad');
 const padButtons = document.querySelectorAll('.pad-button');
 let display = document.getElementById('calc-display');
+let workingValue = '';
+let storedValue = 0;
+let operator = '';
+let solution = 0;
 
 padButtons.forEach(el => {
     el.addEventListener('click', () => {
@@ -12,12 +16,55 @@ padButtons.forEach(el => {
 
 function calcFill (value, type) {
     if(type.contains("num")){
-        console.log('Number selected');
+        workingValue += value;
+        display.textContent = workingValue;
+        console.log(workingValue);
     } else if(type.contains("operator")){
-        console.log('Operator selected');
+        if(workingValue != 0) {
+            storedValue = workingValue;
+        }
+        workingValue = '';
+        operator = value;
     } else if(type.contains("equals")){
-        console.log('Equals selected');
+        solution = calcSolve(storedValue, workingValue, operator);
+        console.log(`var solution is ${solution}`);
+        display.textContent = solution;
+        calcClear(1);
     } else if(type.contains("clear")){
+        calcClear(2);
         console.log('Clear selected');
+    }
+}
+
+function calcSolve(a, b, sign) {
+    a = Number(a);
+    b = Number(b);
+    switch(sign) {
+        case '*':
+            return (a*b).toFixed(0);
+        case '/':
+            return(a/b).toFixed(0);
+        case '+':
+            return(a+b);
+        case '-':
+            return(a-b);
+    }
+}
+
+function calcClear(option) {
+    switch(option){
+        case 1:
+            workingValue = ''
+            storedValue = solution;
+            operator = '';
+            break;
+        case 2:
+            workingValue = ''
+            storedValue = 0;
+            operator = '';
+            display.textContent = 0; 
+            break;
+        default:
+            console.log('Something is wrong...');
     }
 }
